66c1cf963d37849925fabfa03b8e6fb2e3a2511b
Nirodie <sqwich1337@gmail.com>
2026-02-17 10:47:47 +0100

    Books endpoints with static repository

---
+++ LittleLibraryAPI/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace LittleLibraryAPI.Models
+{
+    public class Book
+    {
+        public  int id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public string author { get; set; }
+    }
+}
---
+++ LittleLibraryAPI/Models/BookRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace LittleLibraryAPI.Models
+{
+    public static class BookRepository
+    {
+        public static List<Book> Books = new List<Book>
+        {
+            new Book { id = 1, title = "The Great Gatsby", genre = "Fiction", author = "F. Scott Fitzgerald" },
+            new Book { id = 2, title = "To Kill a Mockingbird", genre = "Fiction", author = "Harper Lee" },
+            new Book { id = 3, title = "1984", genre = "Dystopian", author = "George Orwell" },
+            new Book { id = 4, title = "Pride and Prejudice", genre = "Romance", author = "Jane Austen" },
+            new Book { id = 5, title = "The Hobbit", genre = "Fantasy", author = "J.R.R. Tolkien" }
+        };
+    }
+}
